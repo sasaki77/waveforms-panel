f3c49f8c3892bf6e284f80a9c0a2e07d7ede3824
Shinya Sasaki <shinya.sasaki@kek.jp>
2025-08-25 05:11:34 +0000

    ENH: show lenear scale data with index

--- src/components/WaveformsPanel.tsx
+++ src/components/WaveformsPanel.tsx
@@ -1,59 +1,67 @@
-import React from 'react';
-import { VizLayout, VizLegend } from '@grafana/ui';
+import React, { useState } from 'react';
+import { VizLayout, VizLegend, Slider, useTheme2 } from '@grafana/ui';
 import { PanelProps } from '@grafana/data';
 import { WaveformsOptions } from 'types';
 import { config, PanelDataErrorView } from '@grafana/runtime';
 
 import { Line } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  LineElement,
-  PointElement,
-  CategoryScale,
-  LinearScale,
-  TimeScale,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+import { Chart as ChartJS, LineElement, PointElement, LinearScale, Tooltip } from 'chart.js';
 import 'chartjs-adapter-date-fns';
 
-ChartJS.register(LineElement, PointElement, CategoryScale, LinearScale, TimeScale, Tooltip, Legend);
+ChartJS.register(LineElement, PointElement, LinearScale, Tooltip);
 
 interface Props extends PanelProps<WaveformsOptions> {}
 
-export const WaveformsPanel: React.FC<Props> = ({ options, data, width, height, fieldConfig, id }) => {
+export const WaveformsPanel: React.FC<Props> = ({ options, data, width, height, fieldConfig, id, onOptionsChange }) => {
+  const [index, setIndex] = useState(0);
+  const theme = useTheme2();
   if (data.series.length === 0) {
     return <PanelDataErrorView fieldConfig={fieldConfig} panelId={id} data={data} needsStringField />;
   }
 
+  const dlen = data.series[0].fields.length - 1;
+
+  const onIndexChange = (value: number) => {
+    setIndex(value >= dlen ? dlen - 1 : value || 0);
+    onOptionsChange({ ...options });
+  };
+
   const datasets: any[] = [];
   const { palette, getColorByName } = config.theme2.visualization;
 
+  const marks = {
+    '0': [data.series[0].fields[1].name],
+    [dlen - 1]: [data.series[0].fields[dlen].name],
+  };
+
   data.series.forEach((series, seriesIndex) => {
-    const timeField = series.fields.filter((f) => f.type === 'time');
-    const valueFields = series.fields.filter((f) => f.type === 'number');
+    const timeField = series.fields[0];
+    const valueFields = series.fields[index + 1];
 
-    const timeValues = Array.from(timeField[0].values) ?? [];
+    const timeValues = Array.from(timeField.values) ?? [];
 
-    valueFields.forEach((field, fieldIndex) => {
-      const valueValues = Array.from(field.values);
-      const dataPoints = timeValues.map((time, i) => ({
-        x: time,
-        y: valueValues[i],
-      }));
+    const valueValues = Array.from(valueFields.values);
+    const dataPoints = timeValues.map((time, i) => ({
+      x: time,
+      y: valueValues[i],
+    }));
 
-      datasets.push({
-        label: `${series.name ?? 'Series'} - ${field.name}`,
-        data: dataPoints,
-        fill: false,
-        borderColor: getColorByName(palette[seriesIndex]),
-        tension: 0.1,
-      });
+    datasets.push({
+      label: `${series.name ?? 'Series'} - ${valueFields.name}`,
+      data: dataPoints,
+      fill: false,
+      borderColor: getColorByName(palette[seriesIndex]),
+      tension: 0.1,
     });
   });
 
   const coptions = {
     responsive: true,
+    // Disable animation
+    animation: {
+      duration: 0,
+    },
+
     maintainAspectRatio: false,
 
     plugins: {
@@ -64,20 +72,21 @@ export const WaveformsPanel: React.FC<Props> = ({ options, data, width, height, 
 
     scales: {
       x: {
-        type: 'time' as const,
+        type: 'linear' as const,
         time: {
           unit: 'minute' as const,
         },
         title: {
           display: true,
-          text: 'Time',
+          text: 'Index',
           color: 'white',
         },
         ticks: {
           color: 'white',
+          stepSize: 1,
         },
         grid: {
-          color: 'white',
+          color: theme.colors.border.weak,
         },
       },
       y: {
@@ -87,9 +96,8 @@ export const WaveformsPanel: React.FC<Props> = ({ options, data, width, height, 
           color: 'white',
         },
         grid: {
-          color: 'white',
+          color: theme.colors.border.weak,
         },
-
         ticks: {
           color: 'white',
         },
@@ -116,8 +124,19 @@ export const WaveformsPanel: React.FC<Props> = ({ options, data, width, height, 
     >
       {(w, h) => (
         <div style={{ width: w, height: h }}>
-          <div style={{ width: w, height: h }}>
+          <div style={{ width: w, height: h - 50 }}>
             <Line data={{ datasets }} options={coptions} />
+          </div>
+          <div style={{ width: w - 300, height: 50, marginLeft: 'auto', marginRight: 'auto' }}>
+            <Slider
+              included={false}
+              marks={marks}
+              max={dlen - 1}
+              min={0}
+              orientation="horizontal"
+              value={0}
+              onChange={onIndexChange}
+            />
           </div>
         </div>
       )}
--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import * as common from '@grafana/schema';
 
 type SeriesSize = 'sm' | 'md' | 'lg';
 
-export interface WaveformsOptions  extends common.OptionsWithLegend {
+export interface WaveformsOptions extends common.OptionsWithLegend {
   text: string;
   showSeriesCount: boolean;
   seriesCountSize: SeriesSize;
